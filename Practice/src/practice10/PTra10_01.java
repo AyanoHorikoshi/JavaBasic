4ffb96790c2ec43da86fdec74790b1dd067b95f6
ayanohorikosi <nontankyun@gmail.com>
2018-03-22 16:13:02 +0900

    コミット

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -30,7 +30,7 @@ public class PTra10_01 {
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
 		us = new User();
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-		System.out.println(us.userid);
+		System.out.println(us.userId);
 		System.out.println(us.userNm);
 		System.out.println(us.mail);
 		System.out.println(us.password);
